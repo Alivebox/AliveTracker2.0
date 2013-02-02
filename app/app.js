ade62135d966fa82e054925c0e0d472ad1027622
Juan Carlos Morales <juank.memphis@gmail.com>
2013-02-02 08:50:41 -0600

    Framework modifications to work with the Sencha CMD build command

--- app/app.js
+++ app/app.js
@@ -1,7 +1,50 @@
+Ext.Loader.setConfig({
+    enabled:true
+});
+Ext.Loader.setPath('Framework', 'framework');
+Ext.require(
+    'Framework.Main'
+);
+
 Ext.application({
 
-    name: 'AliveTracker',
+    name:'AliveTracker',
 
-    autoCreateViewport: true
+    autoCreateViewport:false,
+
+    launch:function () {
+        Framework.Main.init({
+            scope:this,
+            localizationEnabled:true,
+            callback:this.onFrameworkInitialized
+        });
+    },
+
+    onFrameworkInitialized:function () {
+        Ext.create('Ext.container.Viewport', {
+            items:[
+                {
+                    xtype:'container',
+                    items:[
+                        {
+                            xtype:'label',
+                            text:'Hello World'
+                        },
+                        {
+                            xtype:'button',
+                            text:Locales.TEST_LABEL,
+                            listeners:{
+                                scope:this,
+                                click:function () {
+                                    Framework.core.LocalizationManager.setLanguageByAbbreviation("en");
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        });
+    }
+
 
 });
---
+++ resources/locales/framework-lang-es.js
@@ -0,0 +1,17 @@
+Ext.define('Locales', {
+
+    statics: {
+
+        //FRAMEWORK CONSTANTS - START
+
+        //FRAMEWORK CONSTANTS - END
+
+        //APPLICATION CONSTANTS - START
+
+        TEST_LABEL: "Cambiar Lenguaje"
+
+        //APPLICATION CONSTANTS - END
+
+    }
+
+});
