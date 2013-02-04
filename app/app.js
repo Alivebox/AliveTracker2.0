e3a666282d10397ac2b4a1de678e1eb823da4203
Juan Carlos Morales <jmorales@alivebox.com>
2013-02-04 01:37:03 +0000

    Loading of configuration file

--- app/app.js
+++ app/app.js
@@ -13,11 +13,47 @@ Ext.application({
     autoCreateViewport:false,
 
     launch:function () {
+        this.loadConfigurationFile();
+    },
+
+    loadConfigurationFile: function(){
+        Ext.Ajax.request({
+            scope:this,
+            url: "resources/fileLoader.php",
+            method:'GET',
+            params:{
+                fileUrl: "config/configFile.json"
+            },
+            success:this.onConfigLoaded,
+            failure:this.onConfigFail
+        });
+    },
+
+    onConfigLoaded: function(argResponse){
+        var tmpConfigFileString = argResponse.responseText;
+        var tmpConfigFileObject = this.decodeConfigFileContent(tmpConfigFileString);
+        if( Ext.isEmpty(tmpConfigFileObject) ){
+            return;
+        }
         Framework.Main.init({
             scope:this,
-            localizationEnabled:true,
+            configFileObject: tmpConfigFileObject,
             callback:this.onFrameworkInitialized
         });
+    },
+
+    decodeConfigFileContent:function (argFileContent) {
+        try {
+            this.configFileObject = Ext.decode(argFileContent);
+        } catch (argError) {
+            var tmpErrorMessage = Ext.util.Format.format(Framework.core.Defaults.FATAL_ERROR_INVALID_CONFIG_FILE,"resources/config/configFile.json");
+            Framework.core.ErrorsManager.handleFatalError(tmpErrorMessage);
+        }
+        return this.configFileObject;
+    },
+
+    onConfigFail:function () {
+        Framework.core.ErrorsManager.handleFatalError(Framework.core.Defaults.FATAL_ERROR_CONFIG_FILE_LOADER_NOT_FOUND);
     },
 
     onFrameworkInitialized:function () {
--- resources/locales/framework-lang-en.js
+++ resources/locales/framework-lang-en.js
@@ -2,15 +2,7 @@ Ext.define('Locales', {
 
 	statics: {
 		
-		//FRAMEWORK CONSTANTS - START
-		
-		//FRAMEWORK CONSTANTS - END
-
-        //APPLICATION CONSTANTS - START
-
         TEST_LABEL: "Change Language"
-
-        //APPLICATION CONSTANTS - END
 
 	}
 	
