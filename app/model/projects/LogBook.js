4068bd9dc433829e8f78c1f19a18524d63d07ce1
Javier <jli@alivebox.com>
2013-03-14 11:29:28 -0600

    Added LogBook

---
+++ app/model/projects/LogBook.js
@@ -0,0 +1,26 @@
+Ext.define('AliveTracker.model.projects.LogBook', {
+
+    extend:'Ext.data.Model',
+
+    idProperty:'id',
+
+    fields:[
+        {
+            name:'date',
+            type:'date',
+            dateFormat: 'Y-m-d'
+        },
+        {
+            name:'group',
+            type:'string'
+        },
+        {
+            name:'activities',
+            type:'array'
+        }
+    ],
+    proxy: {
+        type: AliveTracker.defaults.WebServices.WEB_SERVICE_TYPE,
+        url: AliveTracker.defaults.WebServices.LOG_CREATE_LOGS
+    }
+});
