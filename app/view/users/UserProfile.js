10c626e5f818ebb35b0845fbd6af2bd3a9eaff6f
julateAlivebox <julate@alivebox.com>
2013-04-16 11:53:31 -0600

    Register New Users

---
+++ app/view/users/UserProfile.js
@@ -0,0 +1,8 @@
+Ext.define('AliveTracker.view.users.UserProfile', {
+
+    extend:'Ext.container.Container',
+    xtype: 'userprofile',
+    initComponent: function() {
+        this.callParent(arguments);
+    }
+});
