cde84cdd36036fdc451f39e359a61f3aa9c56243
pgonzalez <pgonzalez@alivebox.com>
2013-05-06 10:36:37 -0600

    style resetPassword view

--- app/view/authentication/ResetPassword.js
+++ app/view/authentication/ResetPassword.js
@@ -1,11 +1,13 @@
 Ext.define('AliveTracker.view.authentication.ResetPassword', {
     extend:'Ext.form.Panel',
     xtype:'resetpasswordform',
+    cls:'reset-password',
 
     initComponent:function () {
         this.items = [
             {
                 xtype:'button',
+                cls:'all-views-button align-button',
                 text: Locales.AliveTracker.RESET_PASSWORD_LABEL,
                 listeners:{
                     scope:this,
